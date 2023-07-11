b8eb87d4ecd3ce0638115cdd78da788b24c014c6
damtrongan <damtrongan@gmail.com>
2023-07-11 23:50:45 +0700

    add

--- 12_Practice/assets/scripts/app.js
+++ 12_Practice/assets/scripts/app.js
@@ -1,26 +1,43 @@
-
 class Tooltip {}
 
 class ProjectItem {
-  constructor(project) {
+  constructor(id) {
+    this.id = id;
+    this.projectItemElement = document.getElementById(this.id);
+    this.connectMoreButton();
+    this.connectSwitchButton();
+  }
+  handlerMoreBtn(){
+    alert("hello")
+  }
 
+  connectMoreButton() {
+    const moreBtn = this.projectItemElement.querySelector('.alt');
+    moreBtn.addEventListener('click', this.handlerMoreBtn)
+  }
+
+  connectSwitchButton() {
+    const finishBtn = this.projectItemElement.querySelector('button:last-of-type');
+    finishBtn.addEventListener('click', this.handlerMoreBtn)
   }
 }
 
 class ProjectList {
+  projects = [];
   constructor(type) {
-    const prjItems = document.querySelector(`#${type}-projects`)
-    console.log(prjItems);
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems){
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject(id) {
     
   }
 
-  connectMoreButton(){
+  switchProject(projectId) {}
 
-  }
-  
-  connectSwitchButton(){
-
-  }
 }
 
 class App {
